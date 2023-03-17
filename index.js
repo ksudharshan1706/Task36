a644f2261b7e75a33d75bd4af6a87bde0678e2d1
ksudharshan1706 <kamavaramsudharshan1706@gmail.com>
2023-03-17 10:50:24 +0530

    update

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 import express from "express";
 import { MongoClient } from "mongodb";
+import dotenv from "dotenv"; //to connect .env contents over here
 //const express = require("express"); type in package.json should be commonjs // importing 3rd party package
 const app = express(); // has all the rest api methods => GET,POST,PUT,DELETE
-
-const PORT = 6000;
+dotenv.config();
+const PORT = process.env.PORT;
 //const MONGO_URL = "mongodb://127.0.0.1"; // this url is responsible for connecting the DB.
-const MONGO_URL =
-  "mongodb+srv://sudharshan:sudharshan@cluster0.vrgbtiq.mongodb.net";
-const client = new MongoClient(MONGO_URL); // dial
-// Top level await
-await client.connect(); // call
-console.log("Mongo is connected !!!  ");
+const MONGO_URL = process.env.MONGO_URL;
+console.log(PORT, MONGO_URL);
+async function createConnection() {
+  const client = new MongoClient(MONGO_URL);
+  await client.connect();
+  console.log("Mongo is Connected");
+  return client;
+}
+
+const client = await createConnection();
 
 app.use(express.json());
+
+app.get("/", async function (req, resp) {
+  const result = await client.db("b39we").collection("Room").find({}).toArray();
+
+  result ? resp.send(result) : resp.status(404).send("Empty Room not Found");
+});
 
 app.post("/CreateRoom", async function (req, resp) {
   const data = req.body;
@@ -41,6 +52,9 @@ app.post("/BookRoom", async function (req, resp) {
     .collection("Room")
     .findOne({ Booked: "False" });
 
+  if (AvailableRoom === null) {
+    resp.send({ Message: "No Rooms Available!! create a room!!" });
+  }
   const updateData = { RoomId: AvailableRoom.id, date: date };
   var result = await client
     .db("b39we")
@@ -82,10 +96,34 @@ app.put("/VacateRoom/:id", async function (req, resp) {
 app.get("/BookedRooms", async function (req, resp) {
   const result = await client
     .db("b39we")
-    .collection("Room")
-    .find({ Booked: "True" })
+    .collection("BookedRoom")
+    .find({})
     .toArray();
 
   result ? resp.send(result) : resp.status(404).send("No rooms are booked!!");
 });
+
+app.get("/BookedRooms/:id", async function (req, resp) {
+  const { id } = req.params;
+  const result = await client
+    .db("b39we")
+    .collection("BookedRoom")
+    .find({ customerName: id })
+    .toArray();
+
+  result ? resp.send(result) : resp.status(404).send("No rooms are booked!!");
+});
+
+app.put("/UpdateAmenities/:id", async function (req, resp) {
+  const { id } = req.params;
+  const amenities = req.body;
+  console.log(id, amenities);
+  const result = await client
+    .db("b39we")
+    .collection("Room")
+    .updateOne({ id: parseInt(id) }, { $set: amenities });
+
+  resp.send(result);
+});
+
 app.listen(PORT, () => console.log(`The server started in: ${PORT} ✨✨`));
